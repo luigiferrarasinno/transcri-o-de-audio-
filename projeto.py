524201742d637f014eefeb5c480bf82347c2c39b
luigiferrarasinno <rm98047@gmail.com.br>
2024-03-02 16:46:31 -0300

    primeiro commit

---
+++ projeto.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import sounddevice as sd
+import soundfile as sf
+import speech_recognition as sr
+
+app = Flask(__name__)
+app.secret_key = "alura"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        duration = int(request.form['duration']) if 'duration' in request.form else 10  # Default duration is 10 seconds
+        fs = 44100
+
+        # Gravação de áudio
+        print("Gravando áudio...")
+        gravacao = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+        sd.wait()
+
+        # Salvar o áudio em um arquivo WAV
+        sf.write('audio_gravado.wav', gravacao, fs)
+
+        # Carregar o arquivo de áudio gravado
+        audio_gravado, _ = sf.read('audio_gravado.wav')
+
+        # Transcrição do áudio
+        recognizer = sr.Recognizer()
+        with sr.AudioFile('audio_gravado.wav') as source:
+            audio_data = recognizer.record(source)
+            try:
+                texto_transcrito = recognizer.recognize_google(audio_data, language='pt-BR')
+            except sr.UnknownValueError:
+                texto_transcrito = "Não foi possível transcrever o áudio."
+            except sr.RequestError as e:
+                texto_transcrito = f"Erro durante o reconhecimento de fala: {e}"
+
+        return render_template('index.html', texto_transcrito=texto_transcrito)
+
+    return render_template('index.html', texto_transcrito=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
